c462c7978c9d554ad276af2814f0bdbcec13bd3f
Carlos Ospino <carlosospinomonsalve@gmail.com>
2022-04-04 02:45:44 +0000

    Updates

---
+++ precache-manifest.4a9b763dcd54d3c6efd45105ce11d7a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef8d2f35eaab75205860",
+    "url": "/pomodoro-react-app/static/css/main.0b76cbda.chunk.css"
+  },
+  {
+    "revision": "ef8d2f35eaab75205860",
+    "url": "/pomodoro-react-app/static/js/main.ef8d2f35.chunk.js"
+  },
+  {
+    "revision": "c0b10e441e8df32a1ba1",
+    "url": "/pomodoro-react-app/static/js/1.c0b10e44.chunk.js"
+  },
+  {
+    "revision": "d50e5e90c41145b1c104",
+    "url": "/pomodoro-react-app/static/js/runtime~main.d50e5e90.js"
+  },
+  {
+    "revision": "0a805ee9789454da5c78e66ade291a14",
+    "url": "/pomodoro-react-app/static/media/tomato.0a805ee9.png"
+  },
+  {
+    "revision": "160c55548fcac2f71bfe43c92372ad99",
+    "url": "/pomodoro-react-app/static/media/15.160c5554.png"
+  },
+  {
+    "revision": "ccc4d6a4c02eb2c63ce584f13b04371e",
+    "url": "/pomodoro-react-app/static/media/30.ccc4d6a4.png"
+  },
+  {
+    "revision": "5bac099c19b6fb43a3936e260b4cb343",
+    "url": "/pomodoro-react-app/static/media/tomato_visible.5bac099c.png"
+  },
+  {
+    "revision": "db02697a344eda78711c35c68f97215e",
+    "url": "/pomodoro-react-app/index.html"
+  }
+];
